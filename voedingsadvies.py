626572374282e97bd1e52c4c5ab2b8fb67ad60b5
Gert-Jan <gertjan1@gmail.com>
2025-04-06 11:12:33 +0200

    🎉 Markdown disclaimer gefixt - hij draait!

--- voedingsadvies.py
+++ voedingsadvies.py
@@ -224,8 +224,12 @@ if advies_output:
         mime="application/pdf"
     )
 
-st.markdown("""---
-*Deze app slaat géén cliëntgegevens op. Alle ingevoerde data verdwijnt zodra het advies is gegenereerd.*""")
+st.markdown(
+    """
+---
+*Deze app slaat géén cliëntgegevens op. Alle ingevoerde data verdwijnt zodra het advies is gegenereerd.*
+"""
+)
 
 
 
